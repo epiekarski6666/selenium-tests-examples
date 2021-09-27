94e9010c148b54974b6779d3da635a215b114291
epiekarski6666 <epiekarski6666@gmail.com>
2021-09-27 22:08:06 +0100

    Initial commit

---
+++ src/test/java/simpleLocators/SimpleLocators.java
@@ -0,0 +1,120 @@
+package simpleLocators;
+
+/*
+By.cssSelector
+By.tagName
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class SimpleLocators {
+
+    WebDriver driver;
+    @BeforeEach
+    public void driverSetup() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @AfterEach
+    public void closeAndQuit() {
+        driver.close();
+        driver.quit();
+    }
+    /*
+    By.className
+    if element class have multiple attributes and you want to use By.className deliberately
+    you need to use kind of below test
+     */
+    @Test
+    public void findElementWithTwoClassNames(){
+        driver.navigate().to("https://the-internet.herokuapp.com/add_remove_elements/");
+        List<WebElement> list = driver.findElements(By.className("columns"));
+        WebElement elementWithTwoClasses = null;
+        for (WebElement tmp: list) {
+            String elementClass = tmp.getAttribute("class");
+            if(elementClass.equals("large-12 columns")){
+                elementWithTwoClasses = tmp;
+            }
+        }
+        Assertions.assertTrue(elementWithTwoClasses != null, "Element with this two phrase class element NOT FOUND");
+    }
+    /*
+    By.id
+     */
+    @Test
+    public void findElementById(){
+        driver.navigate().to("https://the-internet.herokuapp.com/add_remove_elements/");
+        String expected = "Add/Remove Elements\n\nAdd Element";
+        String actual = driver.findElement(By.id("content")).getText();
+        Assertions.assertEquals(expected, actual);
+    }
+    /*
+    By.name
+    */
+    @Test
+    public void findElementByName(){
+        driver.navigate().to("https://the-internet.herokuapp.com/login");
+        WebElement input = driver.findElement(By.name("username"));
+        Assertions.assertTrue(input != null, "Element with this name NOT FOUND");
+    }
+    /*
+    By.linkText
+    */
+    @Test
+    public void findElementByLinkText(){
+        driver.navigate().to("https://the-internet.herokuapp.com/dynamic_loading");
+        String expected = "Example 1: Element on page that is hidden";
+        String actual = driver.findElement(By.linkText("Example 1: Element on page that is hidden")).getText();
+        Assertions.assertEquals(expected, actual);
+    }
+    /*
+    By.partialLinkText
+     */
+    @Test
+    public void findElementByPartialLinkText(){
+        driver.navigate().to("https://the-internet.herokuapp.com/dynamic_loading");
+        String expected = "Example 1: Element on page that is hidden";
+        String actual = driver.findElement(By.partialLinkText("Example 1: Element ")).getText();
+        Assertions.assertEquals(expected, actual);
+    }
+    /*
+    By.xpath
+     */
+    @Test
+    public void findElementByXpath1(){
+        driver.navigate().to("https://the-internet.herokuapp.com/login");
+        WebElement input = driver.findElement(By.xpath("//input[@id='username']"));
+        Assertions.assertTrue(input.isDisplayed(), "Element with this name NOT FOUND");
+    }
+    /*
+    By.xpath
+    xpath combined with two html elemnts with attributes
+    */
+    @Test
+    public void findElementByXpath2(){
+        driver.navigate().to("https://the-internet.herokuapp.com/login");
+        WebElement input = driver.findElement(By.xpath(".//div[@class='large-6 small-12 columns']//input[@id='username']"));
+        Assertions.assertTrue(input.isDisplayed() , "Element with this name NOT FOUND");
+    }
+    /*
+    By.xpath
+    using starts-with
+    */
+    @Test
+    public void findElementByXpath3(){
+        driver.navigate().to("https://the-internet.herokuapp.com/login");
+        WebElement input = driver.findElement(By.xpath(".//div[starts-with(@class, 'large-6')]"));
+        Assertions.assertTrue(input.isDisplayed() , "Element with this name NOT FOUND");
+    }
+
+}
